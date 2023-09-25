410360f96902db6e6e7d76d0b92c5ac67ec29ff5
승범 김 <wewe231223@kpu.ac.kr>
2023-09-25 17:24:51 +0900

    EventHandler 작성 및 좌우 반전 그리기

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -2,22 +2,17 @@ from pico2d import *
 import string
 from enum import Enum, auto
 
-BackGround_Width,BackGround_Height = 1280, 1024
-
-
+BackGround_Width, BackGround_Height = 1280, 1024
 
 open_canvas(BackGround_Width, BackGround_Height)
 
 BackGround = load_image("TUK_GROUND.png")
 
-
-
-
 running = True
 
-class Image:
-    def __init__(self,path = string,frame = int,width = int,height = int):
 
+class Image:
+    def __init__(self, path=string, frame=int, width=int, height=int):
         # 이미지 이름
         self.Path = path
         # 스프라이트의 총 프레임 수
@@ -25,43 +20,80 @@ class Image:
         # 스프라이트의 단위 가로 길이
         self.Width = width
 
-        #스프라이트의 단위 세로 길이
+        # 스프라이트의 단위 세로 길이
         self.Height = height
 
 
 class Behavior(Enum):
-    pass
+    Idle = auto()
+    Run = auto()
+
 
 class Character:
-    def __init__(self,DefaultImg = Image):
+    def __init__(self, DefaultImg=Image):
         self.IdleImage = DefaultImg
+        self.RunImage = None
         self.CurrentImage = DefaultImg
         self.Object = load_image(DefaultImg.Path)
         self.FrameCount = 0
+        self.Direction = 0
+        self.x = 400
+        self.y = 400
+        self.isComposite = False
 
-    def ChangeBehavior(self):
-        pass 
+    def ChangeBehavior(self, TargetBehavior=Behavior):
+        if TargetBehavior == Behavior.Idle:
+            self.FrameCount = 0
+            if self.IdleImage is not None:
+                self.CurrentImage = self.IdleImage
+
+        elif TargetBehavior == Behavior.Run:
+            self.FrameCount = 0
+            if self.RunImage is not None:
+                self.CurrentImage = self.RunImage
+
+
+    def EventHandler(self):
+        events = get_events()
+        for event in events:
+            if event.type == SDL_KEYDOWN:
+                pass
 
 
 
 
-    def Draw(self,x = int,y = int,Scale = int):
+
+
+
+    def Draw(self,Scale=int):
 
         self.FrameCount = (self.FrameCount + 1) % self.CurrentImage.Frame
+        self.x += self.Direction * 5
 
-        self.Object.clip_draw(
-            self.CurrentImage.Width * self.FrameCount,
-            0,
-            self.CurrentImage.Width,
-            self.CurrentImage.Height,
-            x,
-            y,
-            150 * Scale,
-            100 * Scale
-        )
-
-
-
+        if not self.isComposite:
+            self.Object.clip_draw(
+                self.CurrentImage.Width * self.FrameCount,
+                0,
+                self.CurrentImage.Width,
+                self.CurrentImage.Height,
+                self.x,
+                self.y,
+                150 * Scale,
+                100 * Scale
+            )
+        elif self.isComposite:
+            self.Object.clip_composite_draw(
+                self.CurrentImage.Width * self.FrameCount,
+                0,
+                self.CurrentImage.Width,
+                self.CurrentImage.Height,
+                0,
+                'h',
+                self.x,
+                self.y,
+                150 * Scale,
+                100 * Scale,
+            )
 
 def HandleEvent():
     global running
@@ -78,13 +110,10 @@ def HandleEvent():
                 return
 
 
-
-
-
 Character_IdleImage = Image("_Idle.png", 10, 120, 80)
-Character_RunImage = Image("_Run.png",10,120,80 )
+Character_RunImage = Image("_Run.png", 10, 120, 80)
 MainCharacter = Character(Character_IdleImage)
-
+MainCharacter.isComposite = True
 FrameNo = 0
 
 # Main Process
@@ -92,20 +121,11 @@ while running:
     clear_canvas()
 
     BackGround.draw(BackGround_Width // 2, BackGround_Height // 2)
-    #Draw between here
-    MainCharacter.Draw(400, 400,4)
+    # Draw between here
 
+    MainCharacter.Draw(4)
 
-
-
-
-
-
-
-    #Draw between here
+    # Draw between here
     delay(0.05)
     HandleEvent()
     update_canvas()
-
-
-
