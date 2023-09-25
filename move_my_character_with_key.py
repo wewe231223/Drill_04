1858292cb23ac2ced40ae8a1160aa7f652834f79
승범 김 <wewe231223@kpu.ac.kr>
2023-09-25 16:27:51 +0900

    화면 종료 조건 추가

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -15,6 +15,20 @@ BackGround = load_image("TUK_GROUND.png")
 running = True
 
 
+def HandleEvent():
+    global running
+
+    events = get_events()
+
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+            return
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
+                return
+
 
 while running:
     clear_canvas()
@@ -22,6 +36,8 @@ while running:
     BackGround.draw(BackGround_Width // 2, BackGround_Height // 2)
     #Draw between here
 
-
+    HandleEvent()
     update_canvas()
+
+
 
