1c21320079d75558cf40a7891c8a108ce5cb650e
승범 김 <wewe231223@kpu.ac.kr>
2023-09-25 16:24:45 +0900

    배경 출력 및 출력 기초 완성

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,3 +1,27 @@
 from pico2d import *
 
 
+BackGround_Width,BackGround_Height = 1280, 1024
+
+
+
+open_canvas(BackGround_Width,BackGround_Height)
+
+BackGround = load_image("TUK_GROUND.png")
+
+
+
+
+running = True
+
+
+
+while running:
+    clear_canvas()
+
+    BackGround.draw(BackGround_Width // 2, BackGround_Height // 2)
+    #Draw between here
+
+
+    update_canvas()
+
